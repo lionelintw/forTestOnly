d8fc3de88df97645484bb7fd6197c059efc5f369
Lionel <lionelintw@gmail.com>
2020-12-28 10:21:02 +0800

    Update unicode_javascript.php

--- unicode_javascript.php
+++ unicode_javascript.php
@@ -1,5 +1,5 @@
 <?php
-$kw = '我abc要錢';
+$kw = '我蓋#@*&abc,:[]{}要錢';
 function unicode_encodeo($str)
 {
     $i = 0;
@@ -30,7 +30,9 @@ function unicode_decodeo($unicode_html)//better
 //echo mb_convert_encoding(pack("H*" , "00006211"),'UTF-8','UCS-4');
 //echo mb_convert_encoding(pack("H*" , "0061"),'UTF-8','UCS-2');
 //$unicode_html = "\u6211\u61\u8981\u9322";
-    $unicode_html1 = preg_replace("/\\\u([0-9a-f]{4})/ie","iconv('UCS-2BE', 'UTF-8', pack('H4', '$1'))", $unicode_html);
+    //$unicode_html1 = preg_replace("/\\\u([0-9a-f]{4})/ie","iconv('UCS-2BE', 'UTF-8', pack('H4', '$1'))", $unicode_html);
+    $unicode_html1 = preg_replace_callback("/\\\u([0-9a-f]{4})/i",function ($m) {return iconv('UCS-2BE', 'UTF-8', pack('H4', $m[1]));}, $unicode_html);//PHP5.5+
+    //$unicode_html1 = preg_replace("/\\\u([\w]{4})/ie","iconv('UCS-2BE', 'UTF-8', pack('H4', '$1'))", $unicode_html);
     return $unicode_html1;
 }
 echo unicode_decodeo(unicode_encodeo($kw));
@@ -56,14 +58,15 @@ function unicode_encodea($name)//better
     }
     return $str;
 }
-$name = '我abc要錢';
+$name = '我蓋#@*&abc,:[]{}要錢';
 $unicode_name=unicode_encodea($name);
 echo '<br>'.$unicode_name.'<br>';
 // 將UNICODE編碼後的內容進行解碼
 function unicode_decodea($name)
 {
     // 轉換編碼，將Unicode編碼轉換成可以瀏覽的utf-8編碼
-    $pattern = '/([\w]+)|(\\\u([\w]{4}))/i';
+	//$pattern = '/([\w])+|(\\\u([\w]{4}))/i';//輸出不含符號
+    $pattern = '/(\\\u([\w]{4}))|([\s\S])/i';
     preg_match_all($pattern, $name, $matches);
     if (!empty($matches))
     {
@@ -79,12 +82,17 @@ function unicode_decodea($name)
                 $c = iconv('UCS-2BE', 'UTF-8', $c);
                 $name .= $c;
             }
+			elseif(strpos($str, '\\') === 0)
+			{
+				$name .= '';
+			}			
             else
             {
                 $name .= $str;
             }
         }
     }
+
     return $name;
 }
 echo unicode_decodea($unicode_name);
